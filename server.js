e44a3deeb89567336684cf6a3769d924eaa53ed1
Jai Parkash <parkashj05@gmail.com>
2023-12-03 11:27:17 +0530

    Securing Private Data with Dotenv in our Backend App

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const express = require("express");
 const router = require("./router/auth-router");
-const connectDB = require("./utils/db")
+const connectDB = require("./utils/db");
 
 const app = express();
 
--- utils/db.js
+++ utils/db.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 
-const URI = "mongodb://127.0.0.1:27017/neohub"
+// const URI = "mongodb://127.0.0.1:27017/neohub"
+const URI = process.env.MONGODBURI;
 
 const connectDB = async ()=>{
     try {
         await mongoose.connect(URI);
-        console.log("Connection successful to DB");
+        console.log("Connection successfully to DB");
     } catch (error) {
         console.error("Database connection failed");
         process.exit(0)
