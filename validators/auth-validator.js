62d076cfa7aa6e94b4e64ea85c02bd4ec22980dd
Jai Parkash <parkashj05@gmail.com>
2024-01-01 12:55:34 +0530

    Modified auth validator for better practice

--- validators/auth-validator.js
+++ validators/auth-validator.js
@@ -1,25 +1,6 @@
 const { z } = require("zod");
 
-// Creating an object schema
-const signupSchema = z.object({
-    username: z.string({required_error: "Username is required"})
-    .trim()
-    .min(3,{message: "Username must be at least must be 3 charactors"})
-    .max(255, {message: "Username must not be more than 255 charactors"}),
-    email: z.string({required_error:"Email is required"})
-    .trim()
-    .email({ message : 'Please enter a valid Email'})
-    .min(3,{message: "Email must be at least must be 3 charactors"})
-    .max(255, {message: "Email must not be more than 255 charactors"}),
-    password: z.string({required_error:"Password is required"})
-    .min(7,{message: "Password must be at least must be 6 charactors"})
-    .max(1024, {message: "Email must not be more than 1024 charactors"}),
-    phone: z.string({ message : 'Phone is required'})
-    .trim()
-    .min(10,{message: "Email must be at least must be 10 charactors"})
-    .max(20, {message: "Email must not be more than 20 charactors"}),
-});
-
+// Creating an object schema -
 const loginSchema = z.object({
     email: z.string({required_error:"Email is required"})
     .trim()
@@ -28,7 +9,20 @@ const loginSchema = z.object({
     .max(255, {message: "Email must not be more than 255 charactors"}),
     password: z.string({required_error:"Password is required"})
     .min(7,{message: "Password must be at least must be 6 charactors"})
-    .max(1024, {message: "Email must not be more than 1024 charactors"}),
+    .max(1024, {message: "Password must not be more than 1024 charactors"}),
 });
+
+//loginSchema.extend is extending email and password from login schema
+const signupSchema = loginSchema.extend({
+    username: z.string({required_error: "Username is required"})
+    .trim()
+    .min(3,{message: "Username must be at least must be 3 charactors"})
+    .max(255, {message: "Username must not be more than 255 charactors"}),   
+    phone: z.string({ message : 'Phone is required'})
+    .trim()
+    .min(10,{message: "Phone must be at least must be 10 charactors"})
+    .max(20, {message: "Phone must not be more than 20 charactors"}),
+});
+
 
 module.exports = {signupSchema, loginSchema};
