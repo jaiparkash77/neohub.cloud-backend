556062dff680afc699b1c291e0375e4ded6037dd
Jai Parkash <parkashj05@gmail.com>
2024-01-02 21:34:27 +0530

    chore: Admin Panel Backend - Fetching All The Contacts Data

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,4 +1,9 @@
 const Users = require("../models/user-model");
+const Contact = require("../models/contact-model");
+
+//  *-----------------------------------
+// Get All Users Logic
+//  *-----------------------------------
 
 const getAllUsers = async (req, res, next)=>{
     try {
@@ -16,4 +21,22 @@ const getAllUsers = async (req, res, next)=>{
     }
 }
 
-module.exports = getAllUsers;
+//  *-----------------------------------
+// Get All Contacts Logic
+//  *-----------------------------------
+
+const getAllContacts = async(req, res, next) =>{
+    try {
+        const contacts = await Contact.find();
+        console.log(contacts);
+
+        if(!contacts || contacts.length === 0){
+            return res.status(404).json({message: "No Contact found!"});
+        }
+        return res.status(200).json(contacts);
+    } catch (error) {
+        next(error);
+    }
+}
+
+module.exports = {getAllUsers, getAllContacts};
